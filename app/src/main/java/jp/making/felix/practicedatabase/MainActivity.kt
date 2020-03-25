a9913db647b299ce3c8bbc6d9a58418b51f453b1
felix925 <45941545+felix925@users.noreply.github.com>
2020-03-25 13:40:43 +0900

    FirstCommit

---
+++ app/src/main/java/jp/making/felix/practicedatabase/MainActivity.kt
@@ -0,0 +1,18 @@
+package jp.making.felix.practicedatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.making.felix.practicedatabase.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/jp/making/felix/practicedatabase/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package jp.making.felix.practicedatabase.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
